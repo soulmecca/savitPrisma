5ea7db60ff60d56f89fbab37f62c759ee287d372
soulmecca <soulmecca@gmail.com>
2019-05-29 23:30:32 -0400

    add createAccount type and resolver

---
+++ src/api/User/createAccount/createAccount.js
@@ -0,0 +1,17 @@
+import { prisma } from "../../../../db/generated/prisma-client";
+
+export default {
+   Mutation: {
+      createAccount: async (_, args) => {
+         const { username, email, firstName, lastName, bio } = args;
+         const user = await prisma.createUser({
+            username,
+            email,
+            firstName,
+            lastName,
+            bio
+         });
+         return user;
+      }
+   }
+};
